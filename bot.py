9c0d5d13279676a3fe883cdfaee418a76b94260c
KagChi <59391215+KagChi@users.noreply.github.com>
2020-08-03 15:42:19 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,17 +7,23 @@ class Bot(commands.Bot):
     def __init__(self):
         super(Bot, self).__init__(command_prefix=['//'])
 
+        self.add_cog(bot(self))
+
     async def on_ready(self):
         print(f'Logged in as {self.user.name} | {self.user.id}')
 
 
+class bot(commands.Cog):
 
+    def __init__(self, bot):
+        self.bot = bot
 
-    @commands.command(name='connect')
-    async def connect_(self, ctx, *, channel: discord.VoiceChannel = None):
-        
+    @commands.command(name='hello')
+    async def hello(self, ctx):
+        await ctx.send('hi')
 
    
 
+
 bot = Bot()
-bot.run('YoUr NiCE ToKEn')
+bot.run('YoUR NiCE ToKEn')
